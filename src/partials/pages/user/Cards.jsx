be78293e7417e6ff3d4aeca7281fcb37b21cc689
Nicklas Tiger <nicklas.tiger@gmail.com>
2025-04-24 10:52:04 +0200

    added bookingoverview and bookingcategorycard

--- src/partials/pages/user/Cards.jsx
+++ src/partials/pages/user/Cards.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import EventCard from "../../../components/Cards/EventCard/EventCard";
 import StatCard from "../../../components/Cards/StatCard/StatCard";
 import ReviewCard from "../../../components/Cards/ReviewCard/ReviewCard";
+import BookingOverviewCard from "../../../components/Cards/BookingOverviewCard/BookingOverviewCard";
+import BookingCategoryCard from "../../../components/Cards/BookingCategoryCard/BookingCategoryCard";
 
 const Cards = () => {
   return (
     <div className="p-6 grid gap-6 md:grid-cols-2">
       <EventCard
-        category="Musikal & Teater"
-        title="Carola på Cirkus"
+        category="Music & Theater"
+        title="Carola at Cirkus"
         location="Cirkus, Djurgården"
         date="Apr 22, 2025"
         time="19:00"
@@ -17,7 +19,7 @@ const Cards = () => {
         price={30}
       />
       <StatCard
-        label="Totala bokningar"
+        label="Total bookings"
         value="55,000"
         icon="📊"
         percentage={12.5}
@@ -28,9 +30,34 @@ const Cards = () => {
         user="Oliver Bennett"
         rating={5}
         date="Apr 20, 2029"
-        comment="Väldigt bra event!"
-        eventTitle="Carola på Cirkus"
+        comment="Really great event!"
+        eventTitle="Carola at Cirkus"
         category="Music"
+      />
+      <BookingOverviewCard />
+
+      <BookingCategoryCard
+        totalBookings={44115}
+        categories={[
+          {
+            name: "Music & Theater",
+            percent: 43,
+            count: 19022,
+            color: "#ff5cf0",
+          },
+          {
+            name: "Sport",
+            percent: 28,
+            count: 12476,
+            color: "#2d2f94",
+          },
+          {
+            name: "Fashion",
+            percent: 29,
+            count: 12617,
+            color: "#dcdcf7",
+          },
+        ]}
       />
     </div>
   );
