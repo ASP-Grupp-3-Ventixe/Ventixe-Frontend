bb4022e18a8eaf85a4d03fdf62100276ef7cab82
kirubelswedin <kirubelswedin@gmail.com>
2025-05-23 11:59:02 +0200

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const isDev = window.location.hostname === "localhost";
 const config = {
-  authServiceUrl: import.meta.env.VITE_AUTH_SERVICE_URL || 'http://localhost:5103',
-  eventServiceUrl: import.meta.env.VITE_EVENT_SERVICE_URL || 'http://localhost:7101',
-  accountServiceUrl: import.meta.env.VITE_ACCOUNT_SERVICE_URL || 'http://localhost:5177',
-  tokenServiceUrl: import.meta.env.VITE_TOKEN_SERVICE_URL || 'http://localhost:7111',
-  mailHandlingServiceUrl: import.meta.env.VITE_MAIL_HANDLING_SERVICE_URL || 'http://localhost:8080',
+  authServiceUrl: import.meta.env.VITE_AUTH_SERVICE_URL,
+  eventServiceUrl: import.meta.env.VITE_EVENT_SERVICE_URL,
+  accountServiceUrl: import.meta.env.VITE_ACCOUNT_SERVICE_URL,
+  tokenServiceUrl: import.meta.env.VITE_TOKEN_SERVICE_URL,
+  mailHandlingServiceUrl: import.meta.env.VITE_MAIL_HANDLING_SERVICE_URL,
 };
 
 export default config;
