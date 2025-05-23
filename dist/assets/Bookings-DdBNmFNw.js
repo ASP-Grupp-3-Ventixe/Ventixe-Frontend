ca945c7b2a0f198ccd88fff8b1539b7c7ff5ce89
kirubelswedin <kirubelswedin@gmail.com>
2025-05-23 14:42:31 +0200

    Delete old API client code, and create new service and client files for authentication, account, mail, and token management.

--- dist/assets/Bookings-DdBNmFNw.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BATPYeiu.js";const i=()=>o.jsx("div",{children:o.jsx("div",{children:"Bookings"})});export{i as default};
--- dist/assets/SignIn-Dw7j46h-.js
+++
@@ -1,1 +0,0 @@
-import{u as n,j as t}from"./index-BATPYeiu.js";const i=()=>{try{const{signIn:s}=n}catch{}return t.jsx("div",{children:"Sign In"})};export{i as default};
