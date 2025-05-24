3efb383b427f578a7729afc08b25e62840a042be
kirubelswedin <kirubelswedin@gmail.com>
2025-05-24 23:55:58 +0200

    Update mailClient baseURL to use hardcoded value

--- src/api/mailClient.js
+++ src/api/mailClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const mailClient = axios.create({
-    baseURL: import.meta.env.VITE_MAIL_HANDLING_SERVICE_URL + "/api/",
+    baseURL: "https://vpm-webapp-h7bzbgfyardcfje3.swedencentral-01.azurewebsites.net/api/",
 });
 
 mailClient.interceptors.request.use(config => {
