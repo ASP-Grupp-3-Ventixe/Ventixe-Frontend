7d125f8829b7c85aac01c9b12797d6fec2f96018
kirubelswedin <kirubelswedin@gmail.com>
2025-05-26 20:06:11 +0200

    clean up the codebase.

--- src/partials/pages/user/inbox/Hooks/useModal.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export function useModal(initial = false) {
-    const [open, setOpen] = useState(initial);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    return { open, handleOpen, handleClose };
-}
